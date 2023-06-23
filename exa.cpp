0779c4522cd63ed17f14d0d19a4416b0488084b8
Smit Thummar <smitthummar28@gmail.com>
2023-06-23 09:52:36 +0530

    23-6-23

--- exa.cpp
+++ exa.cpp
@@ -3,10 +3,9 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"hello world.";
     cout<<"enter a:";
     cin>>a;
-    cout<<"enter c:";
+    cout<<"enter b:";
     cin>>b;
     int t = a * b;
     cout<<"product is:"<<t;
---
+++ result.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,fail,marks,total,per,acc,stat,eco;
+    cout<<"enter the acc marks:";
+    cin>>acc;
+    cout<<"enter the stat marks:";
+    cin>>stat;
+    cout<<"enter the eco marks:";
+    cin>>eco;
+    total=acc+stat+eco;
+    per=total/3;
+    cout<<"subjects total is:"<<total;
+    cout<<"\nper is:"<<per;
+    if(acc<33||stat<33||eco<33)
+    {
+        cout<<"\nfail....";
+    }
+    else if(per>=80)
+    {
+        cout<<"\nGarde:A";
+    }
+    else if(65<=per && per<80)
+    {
+        cout<<"\nGarde:B";
+    }
+    else if(45<=per && per<65)
+    {
+        cout<<"\nGarde:C";
+    }
+    else if(33<=per && per<45)
+    {
+        cout<<"\nGarde:D";
+    }
+}
