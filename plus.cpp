0a4c3dd7b484791482f9d7ccaff6255c3ef766c6
Smit Thummar <smitthummar28@gmail.com>
2023-07-03 09:44:48 +0530

    3-7-23

---
+++ plus.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class box
+{
+    double l,w,h;
+    public:
+    void setdata(double a,double b,double c)
+    {
+        l=a;
+        w=b;
+        h=c;
+    }
+    double getdata()
+    {
+        return l * w * h;
+    }
+    box operator + (box &n)
+    {
+        box t;
+        t.l = l + n.l;
+        t.w = w + n.w;
+        t.h = h + n.h;
+        return t;
+    }                                                                              
+};
+int main()
+{
+    box a,b,c;
+    a.setdata(3,6,9);
+    cout<<"box a volume is:"<<a.getdata()<<endl;
+    b.setdata(2,4,8);
+    cout<<"box b volume is:"<<b.getdata()<<endl;
+    c = a + b;
+    cout<<"box c volume is:"<<c.getdata()<<endl;
+}
