a77131a0e2246b18c000a5d12f6e0333458c8b82
Smit Thummar <smitthummar28@gmail.com>
2023-07-04 09:01:09 +0530

    4-7-23

---
+++ pre_dic.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class box
+{
+    double l,w,h;
+    public:
+    void setdata(double a,double b,double c)
+    {
+        l=a;
+        w=b;
+        h=c;
+    }
+    double getdata()
+    {
+        return l * w * h;
+    }
+    box operator++ (int)
+    {
+        box t;
+        t.l = ++l;
+        t.w = ++w;
+        t.h = ++h;
+        return t;
+    }
+};
+int main()
+{
+    box a,b;
+    a.setdata(2,5,8);
+    cout<<"box a volume is:"<<a.getdata()<<endl;
+    b=a++;
+    cout<<"box b volume is:"<<b.getdata()<<endl;
+}
